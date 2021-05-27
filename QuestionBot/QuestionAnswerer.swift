4d59fee5f3ec168b0e1680281258284cd2c7198c
Azhibek Abdrakhmanov <azhik459@icloud.com>
2021-05-27 15:46:19 +0600

    Brain is made

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,20 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response
-        return "?"
+        let loweredQuestion = question.lowercased()
+        let answers = ["Wha?", "What do you want?", "I don't understand"]
+        
+        if loweredQuestion == "where are the cookies?" {
+            return "In the cookie Jar!"
+        } else if loweredQuestion.hasPrefix("hello") {
+            return "Well, Hello there!"
+        } else if loweredQuestion.hasPrefix("where")  {
+            return "Th the NORTH!"
+        } else if loweredQuestion.hasPrefix("can") {
+            return "No, can't"
+        } else if loweredQuestion.hasPrefix("should") {
+            return "Yes, definitely"
+        } else {
+            return answers.randomElement()!
+        }
     }
 }
